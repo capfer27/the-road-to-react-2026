d8146a11a4624ab83bb70b2b670ecd5ab8423187
capfer27 <capfer27@gmailcom>
2026-03-17 21:22:31 +0400

    working with list of items

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,54 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-const welcome = {
-  greeting: 'Hey',
-  title : 'React'
-}
+const numbers = [1, 2 , 3, 4, 5];
 
-function getTitle(title) {
-  return title;
-}
+const exponentialNumbers = numbers.map(number => {
+    return number * number;
+});
+
+// console.log(exponentialNumbers)
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://react.dev/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
-    <div >
-      <h1>
-        {welcome.greeting} {welcome.title}. {getTitle('This is a new custom title')}
-      </h1>
+    <div>
+        <h1>My Hacker Stories</h1>
+        <label htmlFor="search">Search: </label>
+        <input type="text" name='search' id='search'/>
 
-      <label htmlFor="search"> Search: </label>
-      <input type="text" id='search'/> 
+        <ul>
+          {list.map( (item, index) => {
+            return (
+               <li key={item.objectID}>
+                  <span>
+                    <a href={item.url}>{item.title}</a>
+                  </span>
+                  <span>{item.author}</span>
+                  <span>{item.num_comments}</span>
+                  <span>{item.points}</span>
+               </li>
+          )})}
+        </ul>
+        <hr />
     </div>
   );
 }
